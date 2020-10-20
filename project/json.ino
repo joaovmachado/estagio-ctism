0e8436b323e5b3226d90d4da61896602a2a77690
João Machado <joao_omachado@outlook.com>
2020-10-20 01:38:20 -0300

    modified project/json.ino - removed luminosity parameter from json string

--- project/json.ino
+++ project/json.ino
@@ -1,2 +1,4 @@
 //JSON que será utilizado na criação de uma requisição em connection.ino
-String json = "{\"Temperatura\": " + (String)dht.readTemperature() + ",\"Umidade\": " + (String)dht.readHumidity() + "Luminosidade: " + convertToLux(analogRead(LDR_PIN)) + ",\"SensorID\":\"1234\"}";
+//String json = "{\"Temperatura\": 24,\"Umidade\": 76,\"SensorID\":\"1234\"}";
+
+String json = "{\"Temperatura\": " + (String)dht.readTemperature() + ",\"Umidade\": " + (String)dht.readHumidity() + ",\"SensorID\":\"1234\"}";
